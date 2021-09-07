ba1ba577ef41d1fca1383da2dbc7dce85f829ea2
typar <typarris421@gmail.com>
2021-09-06 23:48:54 -0400

    models.py

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+STATUS = ((0, 'Draft'), (1, 'Publish'))
+# Create your models here.
+
+# model is a specific class dev'ed by django devs
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    status = models.IntegerField(choices=STATUS, default=0)
