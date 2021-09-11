720190b930ea2c0b7f3f7a4a088a0ed194e00875
typar <typarris421@gmail.com>
2021-09-10 21:46:19 -0400

    added bootstrap

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
+
+# inherit from the admin models
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date_created', 'author')
+
 
 # Register your models here.
+admin.site.register(Post, PostAdmin)
--- mysite/settings.py
+++ mysite/settings.py
@@ -15,6 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
